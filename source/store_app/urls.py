8041abf9ea8ad1dc44ae12fffa9e3ca77a3600ed
Alexandr Toropov <bligs100@gmail.com>
2022-09-27 18:21:15 +0600

    В файл base.html добавлены именованные ссылки на домашнюю страницу и на страницу добавления продуктов
    Создан шаблон добавления продуктов add_product.html
    
    В файл products.py добавлена product_add_view
    
    В файл urls.py добавлены новые адреса, а также имена вьюх

--- source/store_app/urls.py
+++ source/store_app/urls.py
@@ -1,9 +1,11 @@
-from store_app.views.products import products_view, display_product
+from store_app.views.products import products_view, display_product, product_add_view
 from django.urls import path
 
 
 
 urlpatterns = [
-    path("", products_view),
-    path("product/<int:pk>", display_product)
+    path("", products_view, name ='index_view'),
+    path("product/<int:pk>", display_product, name='product_view'),
+    path('product/', products_view),
+    path('product/add', product_add_view, name='add_product')
 ]
--- source/store_app/views/products.py
+++ source/store_app/views/products.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+import re
+from unicodedata import category
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from store_app.models import Categories, Goods
 
 
@@ -16,3 +18,28 @@ def display_product(request, pk):
     return render(request, 'product.html', context = {
         'good': good
     })
+
+
+def product_add_view(request):
+    if request.method == 'GET':
+        print('Method - GET')
+        categories = Categories.objects.all()
+        context = {
+            'categories': categories
+        }
+        return render(request, 'add_product.html', context=context)
+    else:
+        product_info = {
+            'good': request.POST.get('good'),
+            'category': request.POST.get('category'),
+            'price': request.POST.get('price'),
+            'pic': request.POST.get('pic'),
+            'description': request.POST.get('description'),
+        }
+        print(request.POST.get('id'))
+        print('Method - POST')
+        # Goods.objects.create(**product_info)
+        category = Categories()
+        category.id = request.POST.get('category')
+        Goods.objects.create(good = product_info['good'], category=category, price = product_info['price'], pic = product_info['pic'], description = product_info['description'])
+        return redirect(reverse('index_view'))
